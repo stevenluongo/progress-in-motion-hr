70b92a66114ffade5fa96428ef3e7255f07177d0
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-09-01 16:53:16 -0400

    install framer-motion & react-intersection-observer

--- components/text/text-body.tsx
+++ components/text/text-body.tsx
@@ -1,19 +1,26 @@
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 import { TextBaseProps } from "./text";
+import { motion } from "framer-motion";
 
-const TextBody: FC<TextBaseProps> = ({ children, className, light }) => {
-  return (
-    <p
-      className={twMerge(
-        `leading-body text-body`,
-        light ? "text-white" : "text-blue-700",
-        className
-      )}
-    >
-      {children}
-    </p>
-  );
-};
+const TextBody = motion(
+  forwardRef<HTMLParagraphElement, TextBaseProps>(function fn(
+    { children, className, light },
+    ref
+  ) {
+    return (
+      <p
+        ref={ref}
+        className={twMerge(
+          `leading-body text-body`,
+          light ? "text-white" : "text-blue-700",
+          className
+        )}
+      >
+        {children}
+      </p>
+    );
+  })
+);
 
 export default TextBody;
--- components/text/text-container.tsx
+++ components/text/text-container.tsx
@@ -1,31 +1,33 @@
-import React, { Fragment } from "react";
-import { FC } from "react";
+import React, { Fragment, forwardRef, FC } from "react";
 import { twMerge } from "tailwind-merge";
 import { TextContainerProps } from "./text";
+import { motion } from "framer-motion";
 
-const TextContainer: FC<TextContainerProps> = ({
-  children,
-  className,
-  light,
-}) => {
-  return (
-    <div
-      className={twMerge(
-        `flex flex-col gap-y-8 w-full items-center md:items-start text-center md:text-left`,
-        className
-      )}
-    >
-      {light
-        ? children.map((child, index) => (
-            <Fragment key={index}>
-              {React.cloneElement(child as React.ReactElement, {
-                light: true,
-              })}
-            </Fragment>
-          ))
-        : children}
-    </div>
-  );
-};
+const TextContainer = motion(
+  forwardRef<HTMLDivElement, TextContainerProps>(function fn(
+    { className, light, children },
+    ref
+  ) {
+    return (
+      <div
+        ref={ref}
+        className={twMerge(
+          `flex flex-col gap-y-8 w-full items-center md:items-start text-center md:text-left`,
+          className
+        )}
+      >
+        {light
+          ? children.map((child, index) => (
+              <Fragment key={index}>
+                {React.cloneElement(child as React.ReactElement, {
+                  light: true,
+                })}
+              </Fragment>
+            ))
+          : children}
+      </div>
+    );
+  })
+);
 
 export default TextContainer;
