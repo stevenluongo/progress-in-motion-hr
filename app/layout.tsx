2bdc94a8ae1bfb240d23fcd3ecb70321eeedb16c
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-12-31 09:52:21 -0500

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Navbar from "@/components/navbar";
+import Head from "next/head";
 config.autoAddCss = false;
 
 const adelia = localFont({
@@ -24,17 +25,39 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" id="root">
-      <title>
-        Hunters Run | Progress in Motion - Discover Our Ongoing Projects
-      </title>
-      <meta
-        name="description"
-        content="Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today."
-      />
-      <meta
-        name="keywords"
-        content="Hunters Run, Country Club, South Florida, Golf, Racquet Sports, Fitness Center, Restaurants, Special Interest Clubs, Residences, Outdoor Pursuits, Dining, Socializing"
-      />
+      <Head>
+        <title>
+          Hunters Run | Progress in Motion - Discover Our Ongoing Projects
+        </title>
+        <meta
+          name="description"
+          key="desc"
+          content="Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today."
+        />
+        <meta
+          name="keywords"
+          content="Hunters Run, Country Club, South Florida, Golf, Racquet Sports, Fitness Center, Restaurants, Special Interest Clubs, Residences, Outdoor Pursuits, Dining, Socializing"
+        />
+        <meta property="og:title" content="Hunters Run | Progress in Motion" />
+        <meta
+          property="og:description"
+          content="Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://progressinmotion.com" />
+        <meta
+          property="og:image"
+          content="https://progress-in-motion.s3.amazonaws.com/26+North/26North1.jpg"
+        />
+        <meta property="og:image:width" content="1920" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:image:alt" content="26 North Restaurant" />
+        <meta property="og:locale" content="en_US" />
+        <meta
+          property="og:site_name"
+          content="Hunters Run | Progress in Motion"
+        />
+      </Head>
       <body
         className={`${kumbhSans.variable} ${prata.variable} ${adelia.variable}`}
       >
--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,6 @@ import MainDiningRoom from "./_assets/main-dining-room";
 import CallToAction from "./_assets/call-to-action";
 import Modal from "./_assets/modal";
 
-export const metadata = {
-  title: "Hunters Run | Progress in Motion",
-  description:
-    "Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today.",
-};
-
 export default function Page() {
   return (
     <>
