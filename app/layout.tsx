687287ff92440423337e7c5d682042bfd5f36453
Steven <53283472+stevenluongo@users.noreply.github.com>
2023-12-31 11:00:30 -0500

    update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Navbar from "@/components/navbar";
-import Head from "next/head";
+import { Metadata } from "next";
 config.autoAddCss = false;
 
 const adelia = localFont({
@@ -18,6 +18,32 @@ const prata = Prata({
   weight: ["400"],
 });
 
+export const metadata: Metadata = {
+  title: "Hunters Run | Progress in Motion",
+  description:
+    "Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today.",
+  keywords:
+    "Hunters Run, Country Club, South Florida, Golf, Racquet Sports, Fitness Center, Restaurants, Special Interest Clubs, Residences, Outdoor Pursuits, Dining, Socializing",
+  openGraph: {
+    title: "Hunters Run | Progress in Motion",
+    description:
+      "Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today.",
+    type: "website",
+    url: "https://progressinmotion.com",
+    images: [
+      {
+        url: "https://progress-in-motion.s3.amazonaws.com/26+North/26North1.jpg",
+        width: 1920,
+        height: 1080,
+        alt: "26 North Restaurant",
+        type: "image/jpeg",
+      },
+    ],
+    siteName: "Hunters Run | Progress in Motion",
+    locale: "en_US",
+  },
+};
+
 export default async function RootLayout({
   children,
 }: {
@@ -25,39 +51,6 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" id="root">
-      <Head>
-        <title>
-          Hunters Run | Progress in Motion - Discover Our Ongoing Projects
-        </title>
-        <meta
-          name="description"
-          key="desc"
-          content="Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today."
-        />
-        <meta
-          name="keywords"
-          content="Hunters Run, Country Club, South Florida, Golf, Racquet Sports, Fitness Center, Restaurants, Special Interest Clubs, Residences, Outdoor Pursuits, Dining, Socializing"
-        />
-        <meta property="og:title" content="Hunters Run | Progress in Motion" />
-        <meta
-          property="og:description"
-          content="Explore the ongoing projects at Hunters Run Country Club, a premier lifestyle destination nestled in South Florida. With 54-holes of championship golf, 30 Racquet Sports' courts, a Fitness Center, 7 restaurants, and over 20 special interest clubs, our 1,649 residences offer endless opportunities for outdoor pursuits, dining, and socializing. Discover our progress in motion and join our vibrant community today."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://progressinmotion.com" />
-        <meta
-          property="og:image"
-          content="https://progress-in-motion.s3.amazonaws.com/26+North/26North1.jpg"
-        />
-        <meta property="og:image:width" content="1920" />
-        <meta property="og:image:height" content="1080" />
-        <meta property="og:image:alt" content="26 North Restaurant" />
-        <meta property="og:locale" content="en_US" />
-        <meta
-          property="og:site_name"
-          content="Hunters Run | Progress in Motion"
-        />
-      </Head>
       <body
         className={`${kumbhSans.variable} ${prata.variable} ${adelia.variable}`}
       >
